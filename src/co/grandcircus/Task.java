b0e66d73edf552ae380f96d6f57b35f77d47341f
adamjsmith15 <ajsmith015@gmail.com>
2019-05-03 16:26:04 -0400

    Initial commit for Week2 Capstone Task List, Currently functioning

---
+++ src/co/grandcircus/Task.java
@@ -0,0 +1,250 @@
+package co.grandcircus;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Task {
+
+	private String memberName;
+	private String taskDescript;
+	private boolean taskComplete;
+	private LocalDate dueDate;
+	private String formattedDate;
+
+	public Task(String memberName, String taskDescription, LocalDate dueDate) {
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		this.memberName = memberName;
+		this.taskDescript = taskDescription;
+		this.dueDate = dueDate;
+		this.taskComplete = false;
+		this.formattedDate = dateFormat.format(dueDate);
+	}
+
+	public Task() {
+
+	}
+
+	public void addTask(LinkedList<Task> taskList, Scanner sc) {
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		System.out.println("ADD TASK");
+		System.out.println("");
+		System.out.print("Team Member Name: ");
+		String name = sc.nextLine();
+		System.out.println("");
+		System.out.print("Task Description: ");
+		String task = sc.nextLine();
+		System.out.println("");
+		LocalDate date = null;
+		while (date == null) {
+			System.out.print("Due Date (MM/dd/yyyy): ");
+			String tempDate = sc.nextLine();
+			try {
+				date = LocalDate.parse(tempDate, dateFormat);
+				} catch (DateTimeParseException e) {
+				System.out.println("Sorry, that's not valid. Please try again.");
+				}
+		}
+		taskList.add(new Task(name, task, date));
+		setFormattedDate(date);
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Task entered!");
+		System.out.println("");
+
+	}
+
+	public void printIntro() {
+		System.out.println("Welcome to the Task Manager!");
+		System.out.println("");
+		System.out.println("");
+	}
+
+	public void listTasks(LinkedList<Task> taskList, boolean choice) {
+		String format = "%-15s %-15s %-15s %-15s \n";
+		if (!taskList.isEmpty()) {
+			if (choice == true) {
+				System.out.printf(format, "Done?", "Due date", "Team Member", "Description");
+				System.out.println("");
+				for (int i = 0; i < taskList.size(); i++) {
+					System.out.print(taskList.get(i));
+					System.out.println("");
+				}
+			} else {
+				String format2 = "%-10s %-15s %-15s %-15s %-15s \n";
+				System.out.printf(format2, "Task #", "Done?", "Due date", "Team Member", "Description");
+				System.out.println("");
+				for (int i = 0; i < taskList.size(); i++) {
+					System.out.printf("%-11d", i + 1);
+					System.out.print(taskList.get(i));
+					System.out.println("");
+				}
+			}
+		} else {
+			System.out.println("There are no tasks at this time.");
+			System.out.println("");
+		}
+	}
+
+	public void displayMenu() {
+
+		String format = "%5s %s \n";
+		System.out.printf(format, "1.", "  List tasks");
+		System.out.printf(format, "2.", "  Add task");
+		System.out.printf(format, "3.", "  Delete task");
+		System.out.printf(format, "4.", "  Mark task complete");
+		System.out.printf(format, "5.", "  Search task list");
+		System.out.printf(format, "6.", "  Quit");
+		System.out.println("");
+		System.out.println("");
+
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public String getTaskDescript() {
+		return taskDescript;
+	}
+
+	public void setTaskDescript(String taskDescript) {
+		this.taskDescript = taskDescript;
+	}
+
+	public boolean isTaskComplete() {
+		return taskComplete;
+	}
+
+	public void selectTask(LinkedList<Task> list, Scanner sc, String prompt, boolean choice) {
+		if (list.isEmpty()) {
+			System.out.println("List is empty");
+		} else {
+			listTasks(list, false);
+			int userEntry = Validator.getInt(sc, prompt);
+			while (userEntry > list.size() || userEntry < 1) {
+				System.out.println("Not a valid entry, please select a task from the list.");
+				System.out.println(" ");
+				listTasks(list, false);
+				userEntry = sc.nextInt();
+			}
+			if (choice == true) {
+				System.out.println(list.get(userEntry - 1));
+				System.out.print("Are you sure you want to mark this task complete? (y/n): ");
+				String userConfirm = sc.next();
+				if (userConfirm.equals("y")) {
+					list.get(userEntry - 1).setTaskComplete();
+				} else {
+
+				}
+			} else {
+				System.out.println(list.get(userEntry - 1));
+				System.out.print("Are you sure you want to delete this task? (y/n): ");
+				String userConfirm = sc.next();
+				if (userConfirm.equals("y")) {
+					list.remove(userEntry - 1);
+				} else {
+
+				}
+			}
+
+		}
+
+	}
+
+	public String searchList(LinkedList<Task> list, Scanner sc, String prompt) {
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		if (list.isEmpty()) {
+			return "List is empty, cannot search";
+		} else {
+			System.out.println(prompt);
+			String userSearch = sc.next();
+			String searchResults = "";
+			String name;
+			while (!(userSearch.equalsIgnoreCase("name") || userSearch.equalsIgnoreCase("date")
+					|| userSearch.equalsIgnoreCase("quit"))) {
+				System.out.println("Please select a search parameter, name or date (quit to return to menu): ");
+				userSearch = sc.next();
+			}
+			if (userSearch.equalsIgnoreCase("name")) {
+				System.out.print("What name would you like to search for? ");
+				name = sc.next();
+				for (int i = 0; i < list.size(); i++) {
+					if (list.get(i).getMemberName().equalsIgnoreCase(name)) {
+						searchResults += list.get(i) + "\n";
+					} else {
+						continue;
+					}
+				}
+				if (searchResults.isEmpty()) {
+					return "No search results return for " + name;
+				} else {
+					String formatStr = "%-15s %-15s %-15s %-15s \n";
+					System.out.println("Here are the search results for " + name + ":");
+					System.out.printf(formatStr, "Done?", "Due date", "Team Member", "Description");
+					
+					return  searchResults;
+				}
+			} else if (userSearch.equalsIgnoreCase("date")) {
+				System.out.print("What date would you like to see the prior tasks for? ");
+				LocalDate date = null;
+				while (date == null) {
+					System.out.print("(MM/dd/yyyy): ");
+					String tempDate = sc.next();
+					try {
+						date = LocalDate.parse(tempDate, dateFormat);
+					} catch (DateTimeParseException e) {
+					System.out.println("Sorry, that's not valid. Please try again.");
+					}
+					for(int i = 0; i < list.size(); i++) {
+						if(list.get(i).getDueDate().isBefore(date)) {
+							searchResults += list.get(i) + "\n";
+						}else {
+							continue;
+						}
+					}
+				}
+				return searchResults;
+			} else {
+				return "Quitting to menu... \n \n";
+			}
+		}
+	}
+
+	public void setTaskComplete() {
+		this.taskComplete = true;
+	}
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+	public void setFormattedDate(LocalDate dueDate) {
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		this.formattedDate = dateFormat.format(dueDate);
+	}
+	public String getFormattedDate() {
+		return formattedDate;
+	}
+
+	// @override
+	public String toString() {
+		String format = "%-15s %-15s %-15s %-15s \n";
+		String list = "";
+		list = String.format(format, isTaskComplete(), getFormattedDate(), getMemberName(), getTaskDescript());
+		return list;
+	}
+
+}
+
---
+++ src/co/grandcircus/TaskApp.java
@@ -0,0 +1,38 @@
+package co.grandcircus;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class TaskApp {
+
+	public static void main(String[] args) {
+		Task task = new Task();
+		LinkedList<Task> taskList = new LinkedList<>();
+		Scanner scan = new Scanner(System.in);
+		int quit = 0;
+		task.printIntro();
+
+		while (quit != 6) {
+			task.displayMenu();
+			quit = Validator.getInt(scan, "What would you like to do? ", 1, 6);
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			if (quit == 1) {
+				task.listTasks(taskList, true);
+			} else if (quit == 2) {
+				task.addTask(taskList, scan);
+			} else if (quit == 3) {
+				task.selectTask(taskList, scan, "Select the task you would like to delete ", false);
+			} else if (quit == 4) {
+				task.selectTask(taskList, scan, "Select the task you would like to mark complete ", true);
+			} else if (quit == 5){
+				System.out.println(task.searchList(taskList, scan, "Would you like to search by name or date? "));
+			}else {
+				System.out.println("Have a great day!");
+			}
+		}
+		scan.close();
+	}
+
+}
